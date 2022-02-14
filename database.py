0955e9dbd4a7742e591fab30da7c1b0e8cff109a
Abdul Elsadig <abdelrahmanelsadig1@gmail.com>
2022-02-14 15:06:09 +0200

    final commit

--- database.py
+++ database.py
@@ -1,8 +1,6 @@
 from pickle import FALSE
 from app import app
 import mysql.connector
-import yaml
-db1 = yaml.load(open('db.yaml'))
 db = mysql.connector.connect(host = 'bjsqk1ojkibcchfv9y3h-mysql.services.clever-cloud.com',
                              user = 'u2bdsf11zbwizsfx', 
                              passwd = 'tzxS4zjfxp8B86kSUGYM',
