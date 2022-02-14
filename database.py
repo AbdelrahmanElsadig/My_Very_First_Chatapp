e66561a6b8ca717b909375b4b23765c548449448
Abdul Elsadig <abdelrahmanelsadig1@gmail.com>
2022-02-14 14:57:47 +0200

    final commit

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from app import app
 import mysql.connector
 import yaml
 db1 = yaml.load(open('db.yaml'))
-db = mysql.connector.connect(host = db1['mysql_host'],
-                             user = db1['mysql_user'], 
-                             passwd = db1['mysql_password'],
-                             database = db1['mysql_db'],
+db = mysql.connector.connect(host = 'bjsqk1ojkibcchfv9y3h-mysql.services.clever-cloud.com',
+                             user = 'u2bdsf11zbwizsfx', 
+                             passwd = 'tzxS4zjfxp8B86kSUGYM',
+                             database = 'bjsqk1ojkibcchfv9y3h',
                              raw = False
                              )
 cur = db.cursor(dictionary = True)
